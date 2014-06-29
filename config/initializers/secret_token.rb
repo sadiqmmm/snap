9678991f6c019395b6ebb669c2e1abd4b7aea452
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-06-29 06:56:34 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snap::Application.config.secret_token = '2fdd52a2edf134cef9f8726aa6e23ab636ce3e3f5bad0c5198ad3161e85788a05828bc1e5cdaa233e140e6b8f97568c168bca45b457a7dc0d54f10f55b6a2a59'
